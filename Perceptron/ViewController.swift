041da0130acaf7835444da08345ee27cc0f6d5b2
Denis Svichkarev <denis.svichkarev@gmail.com>
2019-06-14 11:03:57 +0300

    Initial commit

---
+++ Perceptron/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Perceptron
+//
+//  Created by Denis Svichkarev on 6/14/19.
+//  Copyright © 2019 Denis Svichkarev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ PerceptronTests/PerceptronTests.swift
@@ -0,0 +1,36 @@
+//
+//  PerceptronTests.swift
+//  PerceptronTests
+//
+//  Created by Denis Svichkarev on 6/14/19.
+//  Copyright © 2019 Denis Svichkarev. All rights reserved.
+//
+
+import XCTest
+@testable import Perceptron
+
+class PerceptronTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
